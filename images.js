b990dd1133acf303ff2695c903b921c5d6dd8722
Philippe Johnson <johnsonphilippe29@gmail.com>
2026-09-21 00:14:26 +0000

    adjusted

--- images.js
+++ images.js
@@ -23,25 +23,5 @@ const IMAGES = [
     annee: "2024",
     categorie: "Corporate",
   },
-  {
-    src: "AFRICA_NEXT_DEBAT.jpg",
-    titre: "Africa Next Débat",
-    lieu: "Lomé",
-    annee: "2025",
-    categorie: "Corporate",
-  },
-  {
-    src: "AFRICA_NEXT_EXPERIENCE.jpg",
-    titre: "Africa Next Experience",
-    lieu: "Agoè",
-    annee: "2025",
-    categorie: "Corporate",
-  },
-  {
-    src: "partenaires.jpg",
-    titre: "Nos partenaires",
-    lieu: "Lomé",
-    annee: "2024",
-    categorie: "Corporate",
-  },
+  
 ];
